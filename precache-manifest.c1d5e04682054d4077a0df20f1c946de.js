5ca1fc642dfe6960fb7553138d18ae28c9fed478
Acid <clockstopfountain@gmail.com>
2019-05-29 06:04:07 +0530

    Updates

---
+++ precache-manifest.c1d5e04682054d4077a0df20f1c946de.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2424e436b1f8b07a22407d0b2e5187",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4edd5799bb8f3463359",
+    "url": "/static/css/main.5ebb8371.chunk.css"
+  },
+  {
+    "revision": "c76acbd8c3450c465efd",
+    "url": "/static/js/2.4cc8969d.chunk.js"
+  },
+  {
+    "revision": "f4edd5799bb8f3463359",
+    "url": "/static/js/main.c9372ea8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c0f99be94075428ce6637ef6dd45e290",
+    "url": "/static/media/beach.c0f99be9.jpg"
+  },
+  {
+    "revision": "8fe8aa2d170b77cc73b5e82c3e61544d",
+    "url": "/static/media/black-and-white-black-and-white-fog-1061623.8fe8aa2d.jpg"
+  },
+  {
+    "revision": "782c29c0f651cd44cf7da65da88175a1",
+    "url": "/static/media/closer.782c29c0.jpg"
+  },
+  {
+    "revision": "9fe96759b533cade69ce7d8224a05ed9",
+    "url": "/static/media/coconut.9fe96759.jpg"
+  },
+  {
+    "revision": "4984e69737b9d853a2d7c833a29ed2b4",
+    "url": "/static/media/fade.4984e697.jpg"
+  },
+  {
+    "revision": "9f23fe0378f586ec8956162e2ab42029",
+    "url": "/static/media/happyBeach.9f23fe03.jpg"
+  },
+  {
+    "revision": "ecc06b4826a6b01bef44b951de4aa4fc",
+    "url": "/static/media/roll.ecc06b48.jpg"
+  },
+  {
+    "revision": "1b9f7993b9578b2e979d2ad1947cc8e1",
+    "url": "/static/media/tim-navis-78745-unsplash.1b9f7993.jpg"
+  },
+  {
+    "revision": "bb867af3029ec0579f2a442043c3dc76",
+    "url": "/static/media/vohoo.bb867af3.jpg"
+  }
+]);
